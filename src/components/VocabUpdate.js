24ed7cac07d772d79ded8b1521bebef55e238173
gadyerg <gadyergonzalez@gmail.com>
2022-12-19 15:19:59 -0700

    can now update vocab list, add cards, and download new file

--- src/components/VocabUpdate.js
+++ src/components/VocabUpdate.js
@@ -1,11 +1,32 @@
-import { Textarea, Input, Box } from '@chakra-ui/react'
+import {
+  Textarea,
+  Input,
+  AccordionItem,
+  AccordionPanel,
+  AccordionButton,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react'
 
 function VocabUpdate(props) {
   return (
-    <Box>
-      <Input value={props.vocabInfo.word} />
-      <Textarea value={props.vocabInfo.definition} />
-    </Box>
+    <AccordionItem>
+      <h2>
+        <AccordionButton>{props.vocabInfo.word}</AccordionButton>
+      </h2>
+      <AccordionPanel>
+        <fieldset>
+          <FormControl isRequired>
+            <FormLabel>Word:</FormLabel>
+            <Input defaultValue={props.vocabInfo.word} />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Definition:</FormLabel>
+            <Textarea defaultValue={props.vocabInfo.definition} />
+          </FormControl>
+        </fieldset>
+      </AccordionPanel>
+    </AccordionItem>
   )
 }
 
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,7 @@
 import List from '../components/List'
 
 function Edit(props) {
-  return <List vocab={props.vocabSet} updateList={props.list} />
+  return <List vocab={props.vocabSet} updateList={props.update} />
 }
 
 export default Edit
